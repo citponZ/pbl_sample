7687798e3a0804d31e3835f7894ad62ebf81bb27
yshirakawa <yuyann971217@gmail.com>
2020-07-09 16:00:18 +0900

    「もっと見る」ボタン設置

--- spring-boot-aks/src/main/resources/static/js/moreLook.js
+++ spring-boot-aks/src/main/resources/static/js/moreLook.js
@@ -1,11 +1,21 @@
-const defaultDispCnt = 2; // 初期表示件数
+$(function(){
+	$('.content:not(.content:first-of-type)').css('display','none');//一番上の要素以外を非表示
+	$('.more').nextAll('.more').css('display','none');//ボタンを非表示
+	$('.more').on('click', function() {
+		$(this).css('display','none');//押したボタンを非表示
+		$(this).next('.content').slideDown('fast');
+		$(this).nextAll('.more:first').css('display','block'); //次のボタンを表示
+	});
+});
+
+/*const defaultDispCnt = 2; // 初期表示件数
 const addDispCnt = 3;     // 追加表示件数
 
 (function ($) {
   $(function () {
     let maxDispCnt = 0;     // 最大表示件数
     let currentDispCnt = 0; // 現在の表示件数
-    let tileList = $('#tileList').children('tr th:each="words : ${wordsList}" th:object="${words}"'); // 一覧のli子要素をすべて取得
+    let tileList = $('#tileList').children('li'); // 一覧のli子要素をすべて取得
 
     // 一覧の初期表示
     $(tileList).each(function (i, elem) {
@@ -35,3 +45,14 @@ const addDispCnt = 3;     // 追加表示件数
           currentDispCnt++;
         }
       });
+
+      // もっと見るボタンを非表示
+      if (maxDispCnt <= newCount) {
+        $(this).hide();
+      }
+
+      return false;
+    });
+  });
+}(jQuery));
+*/
