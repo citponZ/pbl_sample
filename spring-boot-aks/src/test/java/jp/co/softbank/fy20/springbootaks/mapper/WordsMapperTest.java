0c9223f06076fd0802c551148334f0e34cae406a
philv5 <leej49@JiminnoMacBook-Pro.local>
2020-07-15 18:38:58 +0900

    履歴mapper追加

--- spring-boot-aks/src/test/java/jp/co/softbank/fy20/springbootaks/mapper/WordsMapperTest.java
+++ spring-boot-aks/src/test/java/jp/co/softbank/fy20/springbootaks/mapper/WordsMapperTest.java
@@ -25,29 +25,32 @@ public class WordsMapperTest {
 
         // 全件検索のテスト
         System.out.println("==== 全件検索 ====");
+        /*
         List<Words> wordsList1 = wordsMapper.findAll();
         for (Words words : wordsList1) {
             System.out.println(words);
         }
+        */
 
         // 更新のテスト
         System.out.println("==== 更新 ====");
-        int checkUpdate = wordsMapper.update("アップデートしました。", 3);
+        //int checkUpdate = wordsMapper.update("アップデートしました。", 3);
         /*if (checkUpdate){
             System.out.println("更新されました。");
         }
         else{
             System.out.println("更新できませんでした。");
         }*/
-        System.out.println(checkUpdate);
+        //System.out.println(checkUpdate);
 
         // 1検索
         System.out.println("==== 1検索 ====");
-        Words rs = wordsMapper.find(3);
-        System.out.println(rs);
+        //Words rs = wordsMapper.find(3);
+        //System.out.println(rs);
 
         // 削除のテスト
         System.out.println("==== 削除 ====");
+        /*
         boolean checkDelete = wordsMapper.delete(3);
         if (checkDelete){
             System.out.println("削除されました。");
@@ -92,6 +95,39 @@ public class WordsMapperTest {
         }
         else{
             System.out.println("nai");
+        }
+        */
+
+        //履歴追加
+        System.out.println("==== 履歴新規追加 ====");
+        wordsMapper.insertAbb("リーダブルコード", "RD");
+        //履歴確認
+        System.out.println("==== 履歴確認 ====");
+        String str = wordsMapper.checkByNameAbb("リーダブルコード", "RD");
+        if (str == null){
+            System.out.println("ありません");
+        }
+        else{
+            System.out.println("履歴："+str);
+        }
+        //履歴削除
+        System.out.println("==== 履歴削除 ====");
+        //boolean checkDelete = wordsMapper.deleteAbb("リーダブルコード", "RD");
+        boolean checkDelete = false;
+        if (checkDelete){
+            System.out.println("削除されました。");
+        }
+        else{
+            System.out.println("削除できませんでした。");
+        }
+        //履歴確認
+        System.out.println("==== 履歴確認 ====");
+        str = wordsMapper.checkByNameAbb("リーダブルコード", "RD");
+        if (str == null){
+            System.out.println("ありません");
+        }
+        else{
+            System.out.println("履歴："+str);
         }
 
 
