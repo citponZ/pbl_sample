0c3f0b847764577b708e29d16aad2a019da64301
citponZ <31799436+citponZ@users.noreply.github.com>
2021-01-08 19:27:30 +0900

    Update sql.sh

--- spring-boot-aks/sql.sh
+++ spring-boot-aks/sql.sh
@@ -1,5 +1,5 @@
 #dockerでsqlサーバーを立ち上げる
-docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=password123@ -p 1460:1433 --name my_sql_server -d microsoft/mssql-server-linux
+docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=password123@ -p 1433:1433 --name my_sql_server -d microsoft/mssql-server-linux
 
 #SQLサーバーにログイン
 sqlcmd -S 172.20.10.9,1460 -U SA -P password123@ << END_OF_INPUT
